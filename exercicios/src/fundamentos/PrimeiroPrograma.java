a6a325bf0d96988681108a49edc9134685defc89
Ewerson Pulquerio <pulquerio.x@gmail.com>
2022-06-02 21:23:50 -0300

    Iniciando projeto Java

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) { //Main é a porta de entada de um programa Java!
+		System.out.println("Como executar um programan o Eclipse ? Ctrl + Shift + F11"); //imprima uma linha no console do sistema 
+		System.out.println("Primeiro programa Parte #01 e #02");
+	}
+}
